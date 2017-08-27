480ca9d62333a71c5f0a90499186a36bc7f59344
Alex Erling <alex.erling@gmail.com>
2017-08-27 16:22:59 -0500

    Completed assignment 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
     function SongPlayer() {
          var SongPlayer = {};
 
+         /**
+         * @desc currentSong
+         * @type {Object}
+
          var currentSong = null;
          /**
          * @desc Buzz object audio file
@@ -29,11 +33,27 @@
              currentSong = song;
           };
 
+          /**
+          * @function playSong
+          * @desc Private function to play new song
+          * @param {Object} song
+          */
+
+          var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+          }
+
+          /**
+          * @function SongPlayer.play
+          * @desc plays current song
+          * @param {Object} song
+          */
+
          SongPlayer.play = function(song) {
            if (currentSong !== song) {
              setSong(song);
-             currentBuzzObject.play();
-             song.playing = true;
+             playSong(song);
 
            } else if (currentSong === song) {
                if (currentBuzzObject.isPaused()) {
@@ -41,6 +61,12 @@
                }
              }
          };
+
+         /**
+         * @function SongPlayer.play
+         * @desc pauses current song}
+         * @param {Object} song
+         */
 
          SongPlayer.pause = function(song) {
              currentBuzzObject.pause();
